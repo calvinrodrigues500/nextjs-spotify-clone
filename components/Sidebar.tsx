593a9b535cc5210a513de548f5f6d4359ddd8816
calvinrodrigues500 <calvinrodrigues500@gmail.com>
2024-05-09 08:18:42 +0530

    Add sidebar and routes

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,12 +1,38 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
+import { BiSearch } from "react-icons/bi";
+import { HiHome } from "react-icons/hi";
 interface SidebarProps {
 	children: React.ReactNode;
 }
 
 
 const Sidebar: React.FC<SidebarProps> = ({ children }) => {
+
+	const pathName = usePathname();
+
+	const routes = useMemo( () => [
+		{
+			label: 'Home',
+			active: pathName !== '/search',
+			href: '/',
+			icon: HiHome
+		},
+		{
+			label: 'Search',
+			active: pathName === '/search',
+			href: '/search',
+			icon: BiSearch
+		}
+	], []);
+
 	return (
-		<div>
-			{children}
+		<div className="">
+			Sidebar!
 		</div>
 	);
 }
+
+export default Sidebar;
